aa928cf7d473a341029a83f801b172efdd43e4f5
Aleksi Seppänen <aleksi@seppanen.dev>
2022-05-01 13:52:47 +0300

    Add config for config file

--- apisender.py
+++ apisender.py
@@ -16,19 +16,22 @@ def patchStreamMetadata(config, value):
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="apisender")
+    parser.add_argument("--input-file",  nargs='+', help="filenames(s) to input to ", required=True, type=str)
+    parser.add_argument("--config-file", help="filenames for config file", required=True, type=str)
+
+    args = parser.parse_args()
+
     configReader = configparser.ConfigParser()
-    configReader.read('config.ini')
+    configReader.read(args.config_file)
 
     config = configReader['CONFIG']
     if not config:
         print('Please give config')
         quit()
 
-    parser = argparse.ArgumentParser(description="apisender")
-    parser.add_argument("--input-file",  nargs='+', help="filenames(s) to input to ", required=True, type=str)
-
-    args = parser.parse_args()
-
+    heart_rates_latest = []
     while True:
         heart_rates = []
         for input_file in args.input_file:
@@ -42,5 +45,8 @@ if __name__ == "__main__":
             except FileNotFoundError:
                 heart_rates.append(0)
 
-        patchStreamMetadata(config, {'heart_rates': heart_rates})
+        if not heart_rates_latest == heart_rates:
+            print(heart_rates)
+            patchStreamMetadata(config, {'heart_rates': heart_rates})
+            heart_rates_latest = heart_rates
         time.sleep(1)
