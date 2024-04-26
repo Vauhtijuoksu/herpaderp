966bab1ea57b7c64614d2c2cd531ba37e321ebe9
Tuomas-pel <81300414+Tuomas-pel@users.noreply.github.com>
2024-04-26 12:41:00 +0300

    Update launch.sh
    Sykemittari 1:n osoite muutettu oikeaksi

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ source $1/venv/bin/activate
 mkdir $1/html
 python3 $1/apisender.py --config-file $1/config.ini --input-file $1/html/sensor1.txt $1/html/sensor2.txt $1/html/sensor3.txt $1/html/sensor4.txt $1/html/sensor5.txt &
 
-python3 $1/monitor.py DB:6B:EA:95:D0:AE --format="{heartrate}" --output-file="$1/html/sensor1.txt" &
+python3 $1/monitor.py CD:44:02:18:7E:60 --format="{heartrate}" --output-file="$1/html/sensor1.txt" &
 python3 $1/monitor.py E0:3E:9B:CF:AC:80 --format="{heartrate}" --output-file="$1/html/sensor2.txt" &
 python3 $1/monitor.py EA:16:35:F9:58:B9 --format="{heartrate}" --output-file="$1/html/sensor3.txt" &
 python3 $1/monitor.py E8:D8:C7:0B:3F:21 --format="{heartrate}" --output-file="$1/html/sensor4.txt" &
